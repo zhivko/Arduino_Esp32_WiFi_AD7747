0a72b6d4701a30405fd73c94ef39ef2186cfa526
zhivko <klemen.zivkovic@gmail.com>
2017-08-20 17:39:29 +0200

    modified:   Arduino_Esp32_WiFi_AD7747/src/Arduino_Esp32_WiFi_AD7747.ino

--- Arduino_Esp32_WiFi_AD7747/src/Arduino_Esp32_WiFi_AD7747.ino
+++ Arduino_Esp32_WiFi_AD7747/src/Arduino_Esp32_WiFi_AD7747.ino
@@ -9,7 +9,7 @@
 // oled 0x3c device is connected via i2c through pins: pin 4 (CLK) and pin 5 (DATA)
 
 //SSD1306 display(0x3c, SDA, CLK);
-
+// Pin 5 is SDA, Pin 4 is SCL
 SSD1306 display(0x3c, 5, 4);
 int8_t pixelYs[100];
 int8_t previousY;
@@ -29,6 +29,8 @@ int8_t chartHeight = 45;
 uint8_t x_offset = 10;
 uint8_t y_offset = 60;
 
+long loop_i = 0;
+
 static inline int8_t sgn(float val) {
  if (val < 0) return -1;
  if (val==0) return 0;
@@ -36,12 +38,18 @@ static inline int8_t sgn(float val) {
 }
 
 //init ad7747
-void readAD7747()
+void Init_AD7747()
 {
   // http://opensourceecology.orglw/images/eJec/Cap_Sensor EmailChain_7-11-2014.pdf
   // http://opensourceecology.org/wiki/PaulLog
+
+  // Pin 2 is SDA, Pin 14 is SCL
+  // Pin 5 is SDA, Pin 4 is SCL
+  Serial.println("Init_AD7747");
+  Wire.begin(5,4); //
+  Wire.setClock(400000); // choose 400 kHz I2C rate
+
   Serial.println("1");
-  Wire.begin(); //sets up i2c for operation
   Wire.beginTransmission(0x48);
   Wire.write(0xBF);
   Wire.write(0x00);
@@ -65,28 +73,33 @@ void readAD7747()
   Wire.write(0x21); //b'00100001' for continuous conversion, arbitrary VTF setting, and mid-range capacitive conversion time
   Wire.endTransmission();
   Wire.beginTransmission(0x48);
-  Wire.write(0x013); //CAP DAC A Register address (Positive pin data)
+  Wire.write(0x0B); //CAP DAC A Register address (Positive pin data)
   Wire.write(0x80); //b'10111111' for enable Cap DAC A
   Wire.endTransmission();
-  Serial.println("Loop wil start"); //test to make sure serial connection is working
+  Serial.println("Init_AD7747...Done.");
 }
 
 void setup() {
 
 	Serial.begin(115200);
 
-	display.init();
+  Init_AD7747();
+
+  //DISPLAY INIT
+  Serial.println("Display init...");
+  display.init();
 	display.flipScreenVertically();
 	display.setTextAlignment(TEXT_ALIGN_LEFT);
 	display.setFont(ArialMT_Plain_16);
+  display.setLogBuffer(5,20);
+  Serial.println("Display init...Done.");
 
-  Serial.println("readAD7747...");
-  readAD7747();
-  Serial.println("readAD7747...Done.");
 }
 
 void readAD7747_loop()
 {
+  Serial.print("LOOP:");
+  Serial.println(loop_i);
   Wire.beginTransmission(0x48); //talking to chip
   Wire.write(byte(0x00)); //status register address
   Wire.endTransmission();
@@ -96,15 +109,15 @@ void readAD7747_loop()
   readycap = Wire.read();
   if ((readycap & 0x1) == 0) // ready?
   {
-    Serial.print(system_get_time());
-    //Serial.printInr Data Ready'):
-    //delay(10);
+    Serial.print(millis());
+    Serial.println("Data Ready");
+    delay(10);
     Wire.beginTransmission(0x48); //arduino asks for data from ad7747
     Wire.write(0x01); //set address point to capacitive DAC register 1
     Wire.endTransmission(); //pointer is set so non we can read the
 
-    //Serial.println(' Data Incoming');
-    //delay(10);
+    Serial.println("Data Incoming");
+    delay(10);
     Wire.requestFrom(0x48, 3, false); //reads data from cap DAC registers 1-3
     while (Wire.available())
     {
@@ -118,14 +131,10 @@ void readAD7747_loop()
       lo = Wire.read();
       capacitance = (hi << 16) + (mid << 8) + lo - 0x800000;
       pf = (float) capacitance * -1 / (float) 0x800000 * 8.192f;
-
-      //Serial.print(system_get_time());
-      //Serial.print(" ");
-      //Serial.println(pf, DEC): //prints the capacitance data in decimal through serial port
-      //Serial.println();
-      //Serial.print(system_get_time());
-      //Serial.println("Loop Done");
-
+      Serial.print(millis());
+      Serial.print(" ");
+      dtostrf(voltage, 5, 2, charBuf);
+      Serial.println(voltage); //prints the capacitance data in decimal through serial port
     }
   }
 
@@ -133,19 +142,21 @@ void readAD7747_loop()
 
 void loop()
 {
+  loop_i = loop_i + 1;
+  readAD7747_loop();
 
-  //readAD7747_loop();
+  //display.clear();
+  //dtostrf(loop_i, 5, 2, charBuf);
+  //display.drawString(0, 0, charBuf);
+  //display.setPixel(0, 0);
+  //display.print(loop_i);
+  //display.display();
+
+/*
   voltage = 0.1234;
-
 	dtostrf(voltage, 5, 2, charBuf);
 	display.clear();
-
 	display.drawString(0, 0, charBuf);
-
 	display.display();
-
-	//dacWrite(analogPin, analogValue);
-
-
-	value = value + deltaValue;
+*/
 }
