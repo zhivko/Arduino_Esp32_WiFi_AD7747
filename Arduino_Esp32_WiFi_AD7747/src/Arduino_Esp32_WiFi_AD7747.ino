d740cce682a65c5def049a7f814316354671dc33
zhivko <klemen.zivkovic@gmail.com>
2017-08-27 22:16:46 +0200

    modified:   Arduino_Esp32_WiFi_AD7747/src/750px-CapSenseV1.png 	modified:   Arduino_Esp32_WiFi_AD7747/src/Arduino_Esp32_WiFi_AD7747.ino 	new file:   Arduino_Esp32_WiFi_AD7747/src/Arduino_Esp32_WiFi_AD7747.ino.old 	new file:   Arduino_Esp32_WiFi_AD7747/src/CapSenseV1.kicad_pcb 	new file:   Arduino_Esp32_WiFi_AD7747/src/CapSenseV1.pro 	new file:   Arduino_Esp32_WiFi_AD7747/src/CapSenseV1.sch 	new file:   Arduino_Esp32_WiFi_AD7747/src/CapSenseV1Gbr.zip 	new file:   Arduino_Esp32_WiFi_AD7747/src/index.html 	new file:   Arduino_Esp32_WiFi_AD7747/src/rose.svg 	new file:   Arduino_Esp32_WiFi_AD7747/src/script.js 	new file:   lib/arduinoWebSockets 	renamed:    Arduino_Esp32_WiFi_AD7747/lib/esp8266-oled-ssd1306 -> lib/esp8266-oled-ssd1306 	modified:   platformio.ini

--- Arduino_Esp32_WiFi_AD7747/src/Arduino_Esp32_WiFi_AD7747.ino
+++ Arduino_Esp32_WiFi_AD7747/src/Arduino_Esp32_WiFi_AD7747.ino
@@ -12,14 +12,122 @@
   //|                                                                                      |
   //|                                                                                      |
   //-----------------------------------------------------------------------------------------
+
+  #if CONFIG_FREERTOS_UNICORE
+  #define ARDUINO_RUNNING_CORE 0
+  #else
+  #define ARDUINO_RUNNING_CORE 1
+  #endif
+
+
+  #include "Arduino.h"
+  #include <Hash.h>
+  #include <WiFi.h>
+
+  #include <WiFiMulti.h>
+  #include <WebSocketsServer.h>
+  WiFiMulti WiFiMulti;
+
+
+
   #include <Wire.h>                  //include the library for i2c communication
   #include "SSD1306.h" // alias for `#include "SSD1306Wire.h"`
+
+  WebSocketsServer webSocket = WebSocketsServer(81);
+  IPAddress ipClient= IPAddress(0,0,0,0);
+
+  #define USE_SERIAL Serial
+
 
    //int capreadybit = (bitRead(TWDR,0));  //initialize variable readybit to be two wire data register's lsb
 
   SSD1306 display(0x3c, 5, 4);
   float value;
   char charBuf[16];
+
+  void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
+
+      switch(type) {
+          case WStype_DISCONNECTED:
+              USE_SERIAL.printf("[%u] Disconnected!\n", num);
+              ipClient = IPAddress(0,0,0,0);
+              break;
+          case WStype_CONNECTED:
+              {
+                  ipClient = webSocket.remoteIP(num);
+                  USE_SERIAL.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ipClient[0], ipClient[1], ipClient[2], ipClient[3], payload);
+
+  				// send message to client
+  				webSocket.sendTXT(num, "Connected");
+              }
+              break;
+          case WStype_TEXT:
+              USE_SERIAL.printf("[%u] get Text: %s\n", num, payload);
+
+              // send message to client
+              // webSocket.sendTXT(num, "message here");
+
+              // send data to all connected clients
+              // webSocket.broadcastTXT("message here");
+              break;
+          case WStype_BIN:
+              USE_SERIAL.printf("[%u] get binary length: %u\n", num, length);
+  #ifdef ESP8266
+              hexdump(payload, length);
+  #endif
+
+              // send message to client
+              // webSocket.sendBIN(num, payload, length);
+              break;
+      }
+
+  }
+
+  void i2cloop(void *pvParameters)
+  {
+    Wire.beginTransmission(0x48);   //talking to chip
+    Wire.write(byte(0x00));                  //status register address
+    Wire.endTransmission();
+    Wire.requestFrom(0x48,1);       //request status register data
+    int readycap;
+    readycap=Wire.read();
+    if((readycap&0x1)==0){                // ready?
+      //Serial.println("Data Ready");
+      //delay(500);
+      //delay(1);
+      Wire.beginTransmission(0x48); //arduino asks for data from ad7747
+      Wire.write(0x01);             //set address point to capacitive DAC register 1
+
+      Wire.endTransmission();       //pointer is set so now we can read the
+
+      //Serial.println("Data Incoming");
+      //delay(500);
+      //delay(1);
+      Wire.requestFrom(0x48,3);     //reads data from cap DAC registers 1-3
+
+      while(Wire.available()){
+        unsigned char hi,mid,lo;      //1 byte numbers
+        long capacitance;            //will be a 3byte number
+        float pf;                    //scaled value of capacitance
+        hi=Wire.read();
+        mid=Wire.read();
+        lo=Wire.read();
+        capacitance=(hi<<16)+(mid<<8)+lo-0x800000;
+        pf=(float)capacitance*-1/(float)0x800000*8.192f;
+        //Serial.print(pf, DEC); //prints the capacitance data in decimal through serial port
+
+        dtostrf(pf, 12, 6, charBuf);
+      	display.clear();
+        display.drawString(0, 0, charBuf);
+
+        uint8_t progress = (int)((pf - (float)8) * (float)10000);
+        display.drawProgressBar(0, 30, 120, 15, progress);
+        display.display();
+     }
+     //Serial.println();
+    }
+  }
+
 
   void setup()
   {
@@ -41,6 +149,13 @@
     Wire.write(0xA0);              //b'10100000' found from datasheet page 16
     Wire.endTransmission();        //ends write cycle
     delay(4);                      // Wait for data to clock out? I'm not 100% sure why this delay is here (or why it's 4ms)
+
+    Wire.beginTransmission(0x48);   // begins write cycle
+    Wire.write(0x08);              //address pointer for temp setup register
+    Wire.write(B10000000);              //b'10000000' found from datasheet page 16
+    Wire.endTransmission();        //ends write cycle
+    delay(4);                      // Wait for data to clock out? I'm not 100% sure why this delay is here (or why it's 4ms)
+
     Wire.beginTransmission(0x48);   //begins transmission again
     Wire.write(0x09);              //address pointer for capacitive channel excitation register
     Wire.write(0x0E);              //recommended value from datasheet
@@ -55,51 +170,42 @@
     Wire.write(0x80);               //b'10111111' for enable Cap DAC A
     Wire.endTransmission();
     Serial.println("Setup ended.");        //test to make sure serial connection is working
+
+    USE_SERIAL.setDebugOutput(true);
+
+    USE_SERIAL.println();
+    USE_SERIAL.println();
+    USE_SERIAL.println();
+
+    for(uint8_t t = 4; t > 0; t--) {
+        USE_SERIAL.printf("[SETUP] BOOT WAIT %d...\n", t);
+        USE_SERIAL.flush();
+        delay(1000);
+    }
+
+    WiFiMulti.addAP("AndroidAP", "Doitman1");
+
+    while(WiFiMulti.run() != WL_CONNECTED) {
+        delay(100);
+    }
+
+    USE_SERIAL.print("[SETUP] IP: ");
+    USE_SERIAL.println(WiFi.localIP());
+
+    webSocket.begin();
+    webSocket.onEvent(webSocketEvent);
+
+    xTaskCreatePinnedToCore(i2cloop, "i2cloop", 4096, NULL, 1, NULL, 0);
   }
 
 
   void loop()
-{
-    Wire.beginTransmission(0x48);   //talking to chip
-    Wire.write(byte(0x00));                  //status register address
-    Wire.endTransmission();
-    Wire.requestFrom(0x48,1);       //request status register data
-    int readycap;
-    readycap=Wire.read();
-    if((readycap&0x1)==0){                // ready?
-      //Serial.println("Data Ready");
-      //delay(500);
-      delay(5);
-      Wire.beginTransmission(0x48); //arduino asks for data from ad7747
-      Wire.write(0x01);             //set address point to capacitive DAC register 1
+  {
+    webSocket.loop();
+    if(ipClient != IPAddress(0,0,0,0))
+      webSocket.broadcastTXT("Analogue 3.2");
 
-      Wire.endTransmission();       //pointer is set so now we can read the
-
-      //Serial.println("Data Incoming");
-      //delay(500);
-      delay(5);
-      Wire.requestFrom(0x48,3);     //reads data from cap DAC registers 1-3
-
-      while(Wire.available()){
-        unsigned char hi,mid,lo;      //1 byte numbers
-        long capacitance;            //will be a 3byte number
-        float pf;                    //scaled value of capacitance
-        hi=Wire.read();
-        mid=Wire.read();
-        lo=Wire.read();
-        capacitance=(hi<<16)+(mid<<8)+lo-0x800000;
-        pf=(float)capacitance*-1/(float)0x800000*8.192f;
-        //Serial.print(pf, DEC); //prints the capacitance data in decimal through serial port
-
-        dtostrf(pf, 12, 6, charBuf);
-      	display.clear();
-        display.drawString(0, 0, charBuf);
-        display.display();
-     }
-     Serial.println();
+    //Serial.println("Loop Done");
+    //Serial.println("  ");
+    //delay(1000);
   }
-
-  //Serial.println("Loop Done");
-  //Serial.println("  ");
-  //delay(1000);
-}
---
+++ Arduino_Esp32_WiFi_AD7747/src/script.js
@@ -0,0 +1,180 @@
+      var currentTimeMs=(new Date()).getTime();
+	  var websocket;
+	  
+	  var output;
+
+		function searchKeyPress2(e)
+		{
+		    // look for window.event in case event isn't passed in
+		    e = e || window.event;
+		    if (e.keyCode == 13)
+		    {
+		        document.getElementById('btn2').click();
+		        return false;
+		    }
+		    return true;
+		}
+		
+		function handleEnableMotors(cb)
+		{
+			if(cb.checked)
+				doSendCommand("enable");
+			else
+				doSendCommand("disable");
+			
+			
+		}
+		
+		function searchKeyPress1(e)
+		{
+		    // look for window.event in case event isn't passed in
+		    e = e || window.event;
+		    if (e.keyCode == 13)
+		    {
+		        document.getElementById('btn1').click();
+		        return false;
+		    }
+		    return true;
+		}		
+
+	  function init()
+	  {
+		output = parent.document.getElementById("output");
+		testWebSocket();	
+	  }
+	  function testWebSocket()
+	  {
+	  	if (location.host != "")
+	  	{
+			//var wsUri = "ws://" + location.host + "/";
+		}
+		else
+		{
+			var wsUri = "ws://192.168.43.211:81/";
+		}
+		websocket = new WebSocket(wsUri);
+		websocket.onopen = function(evt) { onOpen(evt) };
+		websocket.onclose = function(evt) { onClose(evt) };
+		websocket.onmessage = function(evt) { onMessage(evt) };
+		websocket.onerror = function(evt) { onError(evt) };		
+	  }
+	  function onOpen(evt)
+	  {
+		writeToScreen("CONNECTED");
+		writeToScreen('<span style="color: blue;">Time: ' + ((new Date()).getTime()-currentTimeMs) +'ms Received: ' + evt.data+'</span>');
+		//setInterval(function() {
+		//	doSend("GiveMeData");
+		//}, 150);	
+	  }
+	  function onClose(evt)
+	  {
+		writeToScreen("DISCONNECTED");
+	  }
+	  function onMessage(evt)
+	  {
+		writeToScreen('<span style="color: blue;">Time: ' + ((new Date()).getTime()-currentTimeMs) +'ms Received: ' + evt.data+'</span>');
+		var res = evt.data.split(' ');
+		if (evt.data.slice(0,1) == 'X' && res.length==5)
+		{
+			document.getElementById("roseSvg").contentDocument.getElementById('PositionX').textContent = res[0];
+			document.getElementById("roseSvg").contentDocument.getElementById('PositionY').textContent = res[1];
+			document.getElementById("roseSvg").contentDocument.getElementById('PositionZ').textContent = res[2];
+			document.getElementById("roseSvg").contentDocument.getElementById('PositionE').textContent = res[3];
+			if(endsWith(res[4],"1"))
+				document.getElementById("enableMotors").checked="checked";
+			else
+				document.getElementById("enableMotors").checked="";
+		}
+		else if(evt.data.slice(0,8) == 'Analogue')
+		{
+			document.getElementById('Analogue').textContent = res[1];
+		}
+		else if(evt.data.slice(0,7) == 'Encoder')
+		{
+			document.getElementById('Encoder').textContent = res[1];
+		}
+	  }
+	  function onError(evt)
+	  {
+		writeToScreen('<span style="color: red;">ERROR:</span> ' + evt.data);
+	  }
+	  function doSend(element)
+	  {
+		//writeToScreen("SENT: " + message); 
+		textToSend = element.value;
+		websocket.send(textToSend);
+	  }
+	  function doSendCommand(textToSend)
+	  {
+		if(websocket!=null)
+			websocket.send(textToSend);
+	  }
+	  function writeToScreen(message)
+	  {
+		// var pre = parent.document.createElement("p");
+		// pre.style.wordWrap = "break-word";
+		// pre.innerHTML = message;
+		// output.appendChild(pre);
+		
+		output.innerHTML = message + "<br>\n" + output.innerHTML;		
+	  }
+	  function doDisconnect()
+	  {
+		var disconnect = document.getElementById("disconnect");
+		disconnect.disabled = true;
+		websocket.close();
+	  }
+
+	function jogXYClick(a)
+	{
+
+		var radios = document.getElementsByName('translateOrOposite');
+		var translate='';
+		for (var i = 0, length = radios.length; i < length; i++) {
+		    if (radios[i].checked) {
+		        translate = radios[i].value;
+		        break;
+		    }
+		}
+		var coord = a.substring(0, 1);
+		var sign = a.substring(1, 2);
+		var value = a.substring(2, a.length) * 10;
+		if (translate=='translate')
+		{
+			if (sign=='+')
+				signOposite='-';
+			else
+				signOposite='+';
+			if (coord == 'X' || coord == 'Y')
+				var command = "X" + sign + value + " Y" + signOposite + value;
+			else
+				var command = "Z" + sign + value + " E" + signOposite + value;
+		}
+		else
+		{
+			if (coord == 'X' || coord == 'Y')
+				if(sign=="-")
+					var command = "X" + sign + value + " Y" + sign + value;
+				else
+					var command = "Y" + sign + value + " X" + sign + value;
+			else
+				if(sign=="-")
+					var command = "E" + sign + value + " Z" + sign + value;
+				else
+					var command = "Z" + sign + value + " E" + sign + value;				
+		}
+		writeToScreen("SENT: " + command); 
+		doSendCommand(command);
+	}
+
+	function jogClick(a)
+	{
+		writeToScreen("SENT: " + a); 
+		doSendCommand(a);		
+	}
+
+	function endsWith(str, suffix) {
+	    return str.indexOf(suffix, str.length - suffix.length) !== -1;
+	}
+
+window.addEventListener("load", init, false);
