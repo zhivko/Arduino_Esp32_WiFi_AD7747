2d8fd41b900a5bb68fd839281100cecdbaa4acc9
zhivko <klemen.zivkovic@gmail.com>
2017-08-30 21:43:29 +0200

    modified:   Arduino_Esp32_WiFi_AD7747/src/Arduino_Esp32_WiFi_AD7747.ino 	modified:   README.md

--- Arduino_Esp32_WiFi_AD7747/src/Arduino_Esp32_WiFi_AD7747.ino
+++ Arduino_Esp32_WiFi_AD7747/src/Arduino_Esp32_WiFi_AD7747.ino
@@ -29,7 +29,6 @@
   WiFiMulti WiFiMulti;
 
 
-
   #include <Wire.h>                  //include the library for i2c communication
   #include "SSD1306.h" // alias for `#include "SSD1306Wire.h"`
 
@@ -83,110 +82,97 @@
 
   }
 
-  void i2cloop(void *pvParameters)
-  {
-
-
-
+void i2cloop(void *pvParameters)
+{
     display.init();
-  	display.flipScreenVertically();
-  	display.setTextAlignment(TEXT_ALIGN_LEFT);
-  	display.setFont(ArialMT_Plain_16);
+    display.flipScreenVertically();
+    display.setTextAlignment(TEXT_ALIGN_LEFT);
 
     Wire.begin(5,4,400000);                   //sets up i2c for operation
-     Wire.beginTransmission(0x48);
-     Wire.write(0xBF);
-     Wire.write(0x00);
-     Wire.endTransmission();
-     delay(4);
-     Wire.beginTransmission(0x48);   // begins write cycle
-     Wire.write(0x07);              //address pointer for cap setup register
-     Wire.write(0xA0);              //b'10100000' found from datasheet page 16
-     Wire.endTransmission();        //ends write cycle
-     delay(4);                      // Wait for data to clock out? I'm not 100% sure why this delay is here (or why it's 4ms)
+    Wire.beginTransmission(0x48);
+    Wire.write(0xBF);
+    Wire.write(0x00);
+    Wire.endTransmission();
+    delay(4);
+    Wire.beginTransmission(0x48);   // begins write cycle
+    Wire.write(0x07);              //address pointer for cap setup register
+    Wire.write(0xA0);              //b'10100000' found from datasheet page 16
+    Wire.endTransmission();        //ends write cycle
+    delay(4);                      // Wait for data to clock out? I'm not 100% sure why this delay is here (or why it's 4ms)
 
-     //temperature compensation
- /*
-     Wire.beginTransmission(0x48);   // begins write cycle
-     Wire.write(0x08);              //address pointer for temp setup register
-     Wire.write(B10000000);
-     Wire.endTransmission();        //ends write cycle
-     delay(4);                      // Wait for data to clock out? I'm not 100% sure why this delay is here (or why it's 4ms)
- */
+    //temperature compensation
+    /*
+    Wire.beginTransmission(0x48);   // begins write cycle
+    Wire.write(0x08);              //address pointer for temp setup register
+    Wire.write(B10000000);
+    Wire.endTransmission();        //ends write cycle
+    delay(4);                      // Wait for data to clock out? I'm not 100% sure why this delay is here (or why it's 4ms)
+    */
 
-     Wire.beginTransmission(0x48);   //begins transmission again
-     Wire.write(0x09);              //address pointer for capacitive channel excitation register
-     Wire.write(B00001111);
-     //Wire.write(0x0E);              //recommended value from datasheet
-     Wire.endTransmission();
-     delay(4);
+    Wire.beginTransmission(0x48);   //begins transmission again
+    Wire.write(0x09);              //address pointer for capacitive channel excitation register
+    Wire.write(B00001111);
+    //Wire.write(0x0E);              //recommended value from datasheet
+    Wire.endTransmission();
+    delay(4);
 
-     Wire.beginTransmission(0x48);
-     Wire.write(0x0A);              //address pointer for the configuration register
-     //Wire.write(0x21);              //b'00100001' for continuous conversion, arbitrary VTF setting, and mid-range capacitive conversion time
-     Wire.write(B01000001);              //b'00100001' for continuous conversion, arbitrary VTF setting, and mid-range capacitive conversion time
-     Wire.endTransmission();
-     delay(4);
+    Wire.beginTransmission(0x48);
+    Wire.write(0x0A);              //address pointer for the configuration register
+    //Wire.write(0x21);              //b'00100001' for continuous conversion, arbitrary VTF setting, and mid-range capacitive conversion time
+    Wire.write(B01000001);              //b'00100001' for continuous conversion, arbitrary VTF setting, and mid-range capacitive conversion time
+    Wire.endTransmission();
+    delay(4);
 
-     Wire.beginTransmission(0x48);
-     Wire.write(0x0B);              //CAP DAC A Register address (Positive pin data)
-     Wire.write(0x80);               //b'10111111' for enable Cap DAC A
-     Wire.endTransmission();
-     delay(4); 
-
+    Wire.beginTransmission(0x48);
+    Wire.write(0x0B);              //CAP DAC A Register address (Positive pin data)
+    Wire.write(0x80);               //b'10111111' for enable Cap DAC A
+    Wire.endTransmission();
+    delay(4);
 
     while(1)
-  {
+    {
+      Wire.beginTransmission(0x48);   //talking to chip
+      Wire.write(byte(0x00));                  //status register address
+      Wire.endTransmission();
+      Wire.requestFrom(0x48,1);       //request status register data
+      int readycap;
+      readycap=Wire.read();
+      if((readycap&0x1)==0)
+      {                // ready?
+          Wire.beginTransmission(0x48); //arduino asks for data from ad7747
+          Wire.write(0x01);             //set address point to capacitive DAC register 1
+          Wire.endTransmission();       //pointer is set so now we can read the
+          Wire.requestFrom(0x48,3);     //reads data from cap DAC registers 1-3
+          while(Wire.available())
+          {
+            unsigned char hi,mid,lo;      //1 byte numbers
+            long capacitance;            //will be a 3byte number
+            float pf;                    //scaled value of capacitance
+            hi=Wire.read();
+            mid=Wire.read();
+            lo=Wire.read();
+            capacitance=(hi<<16)+(mid<<8)+lo-0x800000;
+            pf=(float)capacitance*-1/(float)0x800000*8.192f;
+            //Serial.print(pf, DEC); //prints the capacitance data in decimal through serial port
 
+            dtostrf(pf, 12, 6, charBuf);
+            display.clear();
+            display.setFont(ArialMT_Plain_16);
+            display.drawString(0, 0, charBuf);
 
+            uint8_t progress = (int)((pf - (float)8) * (float)10000);
+            display.drawProgressBar(0, 22, 120, 15, progress);
+          }
+      }
+      display.setFont(ArialMT_Plain_10);
+      String stringOut = "IP: " + WiFi.localIP().toString();
+      display.drawString(0, 40, stringOut);
 
+      String stringOut2 = "SSID: " + WiFi.SSID();
+      display.drawString(0, 50, stringOut2);
 
-
-
-
-
-    Wire.beginTransmission(0x48);   //talking to chip
-    Wire.write(byte(0x00));                  //status register address
-    Wire.endTransmission();
-    Wire.requestFrom(0x48,1);       //request status register data
-    int readycap;
-    readycap=Wire.read();
-    if((readycap&0x1)==0){                // ready?
-      //Serial.println("Data Ready");
-      //delay(500);
-      //delay(1);
-      Wire.beginTransmission(0x48); //arduino asks for data from ad7747
-      Wire.write(0x01);             //set address point to capacitive DAC register 1
-
-      Wire.endTransmission();       //pointer is set so now we can read the
-
-      //Serial.println("Data Incoming");
-      //delay(500);
-      //delay(1);
-      Wire.requestFrom(0x48,3);     //reads data from cap DAC registers 1-3
-
-      while(Wire.available()){
-        unsigned char hi,mid,lo;      //1 byte numbers
-        long capacitance;            //will be a 3byte number
-        float pf;                    //scaled value of capacitance
-        hi=Wire.read();
-        mid=Wire.read();
-        lo=Wire.read();
-        capacitance=(hi<<16)+(mid<<8)+lo-0x800000;
-        pf=(float)capacitance*-1/(float)0x800000*8.192f;
-        //Serial.print(pf, DEC); //prints the capacitance data in decimal through serial port
-
-        dtostrf(pf, 12, 6, charBuf);
-      	display.clear();
-        display.drawString(0, 0, charBuf);
-
-        uint8_t progress = (int)((pf - (float)8) * (float)10000);
-        display.drawProgressBar(0, 30, 120, 15, progress);
-        display.display();
-     }
-     //Serial.println();
+      display.display();
     }
-  }
 }
 
 
@@ -194,6 +180,7 @@
   {
     Serial.begin(115200);             // we will monitor this via serial cable
 
+    xTaskCreatePinnedToCore(i2cloop, "i2cloop", 4096, NULL, 1, NULL, 0);
 
     Serial.println("Setup ended.");        //test to make sure serial connection is working
 
@@ -209,10 +196,11 @@
         delay(1000);
     }
 
+    WiFiMulti.addAP("linksys", "Doitman1");
     WiFiMulti.addAP("AndroidAP", "Doitman1");
-
+    WiFiMulti.addAP("TP-LINK_7006", "klemendarja");
     while(WiFiMulti.run() != WL_CONNECTED) {
-        delay(100);
+        delay(1000);
     }
 
     USE_SERIAL.print("[SETUP] IP: ");
@@ -220,8 +208,6 @@
 
     webSocket.begin();
     webSocket.onEvent(webSocketEvent);
-
-    xTaskCreatePinnedToCore(i2cloop, "i2cloop", 4096, NULL, 1, NULL, 0);
   }
 
 
--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # D-duino-32(ESP32&0.96OLED display) & AD7747
 
 This is simple Arduino 1.8 sketch that runs on https://www.tindie.com/products/lspoplove/d-duino-32esp32-and-096oled-display/
-and uses graphic arduino lib for oled: https://github.com/squix78/esp8266-oled-ssd1306
+and uses:
+graphic arduino lib for oled (https://github.com/squix78/esp8266-oled-ssd1306)
+websockets library (https://github.com/bbx10/arduinoWebSockets)
+hash library required by websockets (https://github.com/bbx10/Hash_tng.git)
 
 Reads distance via capacity sensor AD7747:
 (reference: http://opensourceecology.org/wiki/Paul_Log#Tue_Jul_30.2C_2013)
